c85f17e3bee04574fae36f5f142cb086c6d8fab2
5dma <mark@lautman.net>
2022-02-07 20:07:34 -0500

    Fix expansion of child widgets.

--- view.c
+++ view.c
@@ -17,7 +17,7 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
 
     g_hash_table_insert(pointer_passer, &KEY_WINDOW, window);
 
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 400);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 600);
 
     GtkWidget *label_csv_file = gtk_label_new("CSV file:");
     GtkWidget *text_filename = gtk_entry_new();
@@ -27,9 +27,9 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
     g_hash_table_insert(pointer_passer, &KEY_TEXT_FILENAME, text_filename);
 
     GtkWidget *hbox_file_choose = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_file_choose), label_csv_file, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_file_choose), text_filename, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_file_choose), button_choose, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_file_choose), label_csv_file, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_file_choose), text_filename, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_file_choose), button_choose, FALSE, FALSE, 10);
 
     GtkWidget *checkbox_has_headers = gtk_check_button_new_with_label("File contains column headers");
     g_hash_table_insert(pointer_passer, &KEY_CHECKBOX_HEADER, checkbox_has_headers);
@@ -38,8 +38,8 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
     g_signal_connect(G_OBJECT(button_go), "clicked", G_CALLBACK(process_file), pointer_passer);
 
     GtkWidget *hbox_headers_go = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_headers_go), checkbox_has_headers, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_headers_go), button_go, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_headers_go), checkbox_has_headers, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_headers_go), button_go, FALSE, FALSE, 10);
 
     GtkListStore *list_store_results = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_INT);
 
@@ -93,8 +93,8 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
     g_signal_connect(G_OBJECT(entry_table_name), "changed", G_CALLBACK(concat_command), pointer_passer);
 
     GtkWidget *hbox_table_name = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_table_name), label_table_name, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_table_name), entry_table_name, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_table_name), label_table_name, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_table_name), entry_table_name, FALSE, FALSE, 10);
 
     GtkWidget *label_mysql_command = gtk_label_new("CREATE TABLE ");
     gtk_label_set_justify(GTK_LABEL(label_mysql_command), GTK_JUSTIFY_LEFT);
@@ -110,8 +110,8 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
     g_signal_connect(G_OBJECT(button_copy), "clicked", G_CALLBACK(copy_statement), pointer_passer);
 
     GtkWidget *hbox_close_copy = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_close_copy), button_close, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(hbox_close_copy), button_copy, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_close_copy), button_close, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(hbox_close_copy), button_copy, FALSE, FALSE, 10);
 
     GtkWidget *status_bar = gtk_statusbar_new();
     g_hash_table_insert(pointer_passer, &KEY_STATUS_BAR, status_bar);
@@ -122,13 +122,13 @@ GtkWidget *make_window(GHashTable *pointer_passer) {
     g_hash_table_insert(pointer_passer, &STATUS_BAR_CONTEXT_INFO_CURRENT_MESSAGE_ID, &status_bar_context_info_message_id);
 
     GtkWidget *vbox_ui = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_file_choose, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_headers_go, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), scrolled_window, FALSE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_table_name, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), label_mysql_command, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_close_copy, TRUE, TRUE, 10);
-    gtk_box_pack_start(GTK_BOX(vbox_ui), status_bar, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_file_choose, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_headers_go, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), scrolled_window, TRUE, TRUE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_table_name, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), label_mysql_command, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), hbox_close_copy, FALSE, FALSE, 10);
+    gtk_box_pack_start(GTK_BOX(vbox_ui), status_bar, FALSE, FALSE, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox_ui);
 
     /* Upon destroying the application, free memory in data structures in pointer_passer. */
